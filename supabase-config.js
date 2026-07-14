d2b1bdcf501dc40d2746892e09fa3fe5fa1f2467
AmmarExp <ammar.exp@outlook.sa>
2026-07-14 22:32:15 +0300

    Update Supabase configuration values

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // ⚠️ استبدل هذه القيم بقيم مشروعك على Supabase
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_URL = 'https://yntbplanuotadiwhmqlz.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_0orm8kUD8X8nZGvWZgm7NA_ddcA4W0p';
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
